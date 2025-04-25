e1923dd2e80c4662d9079dc71b1067d5fff76836
yusakkozy <yusak.s@kozystay.com>
2025-04-25 10:09:42 +0700

    fix - fizzbuzz not printed correctly

--- daily01_fizzbuzz/main.go
+++ daily01_fizzbuzz/main.go
@@ -14,12 +14,12 @@ import (
 
 func main() {
 	for i := 1; i <= 100; i++ {
-		if i%3 == 0 {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
 			fmt.Println("Fizz")
 		} else if i%5 == 0 {
 			fmt.Println("Buzz")
-		} else if i%3 == 0 && i%5 == 0 {
-			fmt.Println("FizzBuzz")
 		} else {
 			fmt.Println(i)
 		}
